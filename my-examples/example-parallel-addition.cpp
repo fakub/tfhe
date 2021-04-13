cc3c1f55176cebdf540c5b1f16e5b5532a3a8697
Jakub Klemsa <jakub.klemsa@fel.cvut.cz>
2021-04-13 16:25:46 +0200

    Parallel addition: first hit works, now test it.

--- my-examples/example-parallel-addition.cpp
+++ my-examples/example-parallel-addition.cpp
@@ -249,15 +249,22 @@ void paral_calc_qi(LweSample *qi,
 
     // aux variables
     LweSample *r1 = new_LweSample(io_lwe_params);
-    //~ LweSample *r2 = new_LweSample(io_lwe_params);
-    //~ LweSample *r3 = new_LweSample(io_lwe_params);
+    LweSample *r2 = new_LweSample(io_lwe_params);
+    LweSample *r3 = new_LweSample(io_lwe_params);
     LweSample *r23= new_LweSample(io_lwe_params);
 
-    //TODO
-    // r1   = w_i   <> +-3
-    // r2   = w_i   == +-2
-    // r3   = w_i-1 <> +-2
-    // r23  = r2+r3 == +-2
+    //            r1:  w_i <=> +-3
+    paral_bs_gleq(r1,  w_i0,     3,     bk);
+
+    //            r2:  w_i == +-2
+    paral_bs_eq  (r2,  w_i0,    2,      bk);
+
+    //            r3:  w_i-1 <=> +-2
+    paral_bs_gleq(r3,  w_i1,       2,   bk);
+
+    //            r23: r2+r3 == +-2
+    lweAddTo(          r2,r3,           io_lwe_params);
+    paral_bs_eq  (r23, r2,        2,    bk);
 
     // q_i = r1 + r23
     lweNoiselessTrivial(qi, 0, io_lwe_params);
@@ -280,13 +287,14 @@ void paral_add(LweSample *z,
     // alloc aux variable arrays for w_i, q_i
     LweSample *w = new_LweSample_array(3, io_lwe_params);
     LweSample *q = new_LweSample_array(2, io_lwe_params);
+    LweSample *tmpz = new_LweSample(io_lwe_params);
 
     // calc w_i = x_i + y_i   for i, i-1, i-2
     for (int i = 0; i < 3; i++)
     {
         lweNoiselessTrivial(w + i, 0, io_lwe_params);
-        lweAddTo(w + i, x + i, io_lwe_params);
-        lweAddTo(w + i, y + i, io_lwe_params);
+        lweAddTo(w + i, x - i, io_lwe_params);   // n.b., w_i-1 is at w + 1 position
+        lweAddTo(w + i, y - i, io_lwe_params);
     }
 
     // calc q_i and q_i-1
@@ -294,16 +302,18 @@ void paral_add(LweSample *z,
     paral_calc_qi(q + 1, w + 1, w + 2, bk);
 
     // calculate result: z_i = w_i - 4q_i + q_i-1
-    lweNoiselessTrivial(z, 0, io_lwe_params);
-    lweAddTo(z, w, io_lwe_params);
-    lweSubMulTo(z, 4, q, io_lwe_params);
-    lweAddTo(z, q + 1, io_lwe_params);
+    lweNoiselessTrivial(tmpz, 0, io_lwe_params);
+    lweAddTo(tmpz, w, io_lwe_params);
+    lweSubMulTo(tmpz, 4, q, io_lwe_params);
+    lweAddTo(tmpz, q + 1, io_lwe_params);
 
+    // bootstrap to refresh noise
+    paral_bs_id(z, tmpz, bk);
+
+    // cleanup
+    delete_LweSample(tmpz);
     delete_LweSample_array(2, q);
     delete_LweSample_array(3, w);
-
-    //TODO more like functional bootstrap: identity [-2 .. 2]
-    //~ tfhe_bootstrap_FFT(result, bk->bkFFT, MU, temp_result);
 }
 
 
@@ -329,61 +339,126 @@ int32_t main(int32_t argc, char **argv)
     const LweParams *io_lwe_params = tfhe_params->in_out_params;
     // generate TFHE secret keys
     TFheGateBootstrappingSecretKeySet *tfhe_keys = new_random_gate_bootstrapping_secret_keyset(tfhe_params);
+
     // alloc samples
-    LweSample *x = new_LweSample(io_lwe_params);
-    //~ LweSample *y = new_LweSample(io_lwe_params);
-    //~ LweSample *r = new_LweSample(io_lwe_params);
+    LweSample *a  = new_LweSample(io_lwe_params);   // for BS testing
     LweSample *id = new_LweSample(io_lwe_params);
     LweSample *gl = new_LweSample(io_lwe_params);
     LweSample *eq = new_LweSample(io_lwe_params);
 
+    const int32_t wordlen = 10;
+    LweSample *x = new_LweSample_array(wordlen,     io_lwe_params);   // for parallel addition
+    LweSample *y = new_LweSample_array(wordlen,     io_lwe_params);
+    LweSample *z = new_LweSample_array(wordlen + 1, io_lwe_params);
+
+
+    // -------------------------------------------------------------------------
+    //
+    //  Bootstrapping Test
+    //
+
     // print table heading
     printf("--------------------------------------------------------------------------------\n");
-    printf(" Encr -> Decr  | Id. | <=> 3 | == 2 |\n");
+    printf(" Encr -> Decr  | Id. | <=> 3 | == 2 | timing (Id.)\n");
     printf("--------------------------------------------------------------------------------\n");
 
-    for (int32_t i = 0; i < 16; i++)
+    for (int32_t i = 0; i < (1 << PI); i++)
+    //~ for (int32_t i = 0; false; i++)
     {
         // encrypt
-        paral_sym_encr_priv(x, i - 8, tfhe_keys);
+        paral_sym_encr_priv(a, i - (1 << (PI-1)), tfhe_keys);
 
         // bootstrap
-        paral_bs_id  (id, x,    &(tfhe_keys->cloud)); //FUCKUP #01: member 'cloud' is a struct, but not a pointer (unlike others)
-        paral_bs_gleq(gl, x, 3, &(tfhe_keys->cloud));
-        paral_bs_eq  (eq, x, 2, &(tfhe_keys->cloud));
+        clock_t begin_id = clock();
+        paral_bs_id(id, a, &(tfhe_keys->cloud)); //FUCKUP #01: member 'cloud' is a struct, but not a pointer (unlike others)
+        clock_t end_id = clock();
+
+        // clock_t begin_gl = clock();
+        paral_bs_gleq(gl, a, 3, &(tfhe_keys->cloud));
+        // clock_t end_gl = clock();
+
+        // clock_t begin_eq = clock();
+        paral_bs_eq(eq, a, 2, &(tfhe_keys->cloud));
+        // clock_t end_eq = clock();
 
         // decrypt
-        int32_t x_plain     = paral_sym_decr(x,  tfhe_keys);
+        int32_t a_plain     = paral_sym_decr(a,  tfhe_keys);
         int32_t id_plain    = paral_sym_decr(id, tfhe_keys);
         int32_t gl_plain    = paral_sym_decr(gl, tfhe_keys);
         int32_t eq_plain    = paral_sym_decr(eq, tfhe_keys);
 
-        printf(" D[E(%+d)] = %+d |  %+d |   %+d  |  %+d  |\n", i-8, x_plain, id_plain, gl_plain, eq_plain);
+        printf(" D[E(%+d)] = %+d |  %+d |   %+d  |  %+d  | %lu ms\n", i-8, a_plain,
+                                    id_plain, gl_plain, eq_plain,
+                                                            (end_id - begin_id) / 1000);
     }
     printf("\n");
 
-    //~ cout << "starting bootstrapping ..." << endl;
 
-    //~ clock_t begin1 = clock();
-    //~ bootsAND(r, x, y, &tfhe_keys->cloud);
-    //~ clock_t end1 = clock();
+    // -------------------------------------------------------------------------
+    //
+    //  Parallel Addition Test
+    //
 
-    //~ cout << "finished bootstrapping, total time " << (end1 - begin1) << " [us]" << endl;
+    // encrypt
+    for (int32_t i = 0; i < wordlen; i++)
+    {
+        paral_sym_encr(x + i,  2, tfhe_keys);
+        paral_sym_encr(y + i, -1, tfhe_keys);
+    }
 
-    // verify
-    //~ int32_t x_plain = paral_sym_decr(x, tfhe_keys);
-    //~ int32_t y_plain = paral_sym_decr(y, tfhe_keys);
-    //~ int32_t r_plain = paral_sym_decr(r, tfhe_keys);
+    // print inputs
+    printf("\n------------------------------------------------------------\n");
 
-    //~ cout << "r = x + y ... " << r_plain << " = " << x_plain << " + " << y_plain << endl;
+    // x
+    printf(" X  | +0 ");
+    for (int32_t i = wordlen - 1; i >= 0; i--)
+    {
+        int32_t plain = paral_sym_decr(x + i, tfhe_keys);
+        printf("| %+d ", plain);
+    }
+    printf("|\n");
 
-    // cleanup
+    // y
+    printf(" Y  | +0 ");
+    for (int32_t i = wordlen - 1; i >= 0; i--)
+    {
+        int32_t plain = paral_sym_decr(y + i, tfhe_keys);
+        printf("| %+d ", plain);
+    }
+    printf("|\n----------------------------------------------------");fflush(stdout);
+
+    // parallel addition & print
+    //TODO corner cases where there are supposed to be zeros
+    for (int32_t i = 2; i < wordlen; i++)
+    {
+        paral_add(z + i, x + i, y + i, &(tfhe_keys->cloud));
+        printf("-");fflush(stdout);
+    }
+
+    // print results
+    // z
+    printf("\n Z  ");
+    for (int32_t i = wordlen; i >= 0; i--)
+    {
+        int32_t plain = paral_sym_decr(z + i, tfhe_keys);
+        printf("| %+d ", plain);
+    }
+    printf("|\n------------------------------------------------------------\n");
+
+
+    // -------------------------------------------------------------------------
+    //
+    //  Cleanup
+    //
+
     delete_LweSample(eq);
     delete_LweSample(gl);
     delete_LweSample(id);
-    //~ delete_LweSample(r);
-    //~ delete_LweSample(y);
-    delete_LweSample(x);
+    delete_LweSample(a);
+
+    delete_LweSample_array(wordlen,     x);
+    delete_LweSample_array(wordlen,     y);
+    delete_LweSample_array(wordlen + 1, z);
 
     delete_gate_bootstrapping_secret_keyset(tfhe_keys);
     delete_gate_bootstrapping_parameters(tfhe_params);
