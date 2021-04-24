b368d89ddce1e0ecc47052adbe4691d02c8a4745
Jakub Klemsa <jakub.klemsa@fel.cvut.cz>
2021-04-24 20:26:36 +0200

    Initialized error hunting code.

---
+++ my-examples/ffnt-error-hunt.cpp
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+#include <cmath>
+#include <sys/time.h>
+#include "tfhe.h"
+#include "polynomials.h"
+#include "lwesamples.h"
+#include "lwekey.h"
+#include "lweparams.h"
+#include "tlwe.h"
+#include "tgsw.h"
+
+using namespace std;
+
+
+
+// =============================================================================
+//
+//  MAIN
+//
+
+int32_t main(int32_t argc, char **argv)
+{
+    return 0;
+}
